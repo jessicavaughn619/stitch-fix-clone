d68a3e4ddd424ea2be42179f0d88a23f2c37c795
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-20 10:24:15 -0600

    Add Browser Router and Home route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import Header from './components/header/Header';
 import HomeHeader from './components/body/HomeHeader';
+import { Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <div className="App">
       <Header />
-      <HomeHeader />
+      <Routes>
+        <Route exact path="/" element={
+          <HomeHeader />}
+          />
+      </Routes>
     </div>
   );
 }
