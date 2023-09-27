693b4a3553c0af286e7cc00e983e1597e8529545
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-27 15:59:11 -0400

    Style faq bullet point, add state to manage expand/collapse state

--- client/src/components/home/faqs/FaqBullet.js
+++ client/src/components/home/faqs/FaqBullet.js
@@ -1,12 +1,25 @@
 import './faqs.css'
+import { useState } from 'react'
 
 export default function FaqBullet({ faq }) {
+    const [isExpand, setIsExpand] = useState(false)
     const { question, answer, moreInfoTitle, moreInfoLink } = faq
+
+    function handleClick() {
+        setIsExpand(isExpand => !isExpand)
+    }
 
     return (
         <div className="faq_bullet">
-            <p className="expand_collapse_icon">+</p>
+            <div className="faq_bullet_collapse">
+            <p className="expand_collapse_icon" onClick={handleClick}>{isExpand ? "-" : "+"}</p>
             <p className="question">{question}</p>
+            </div>
+            {isExpand ? 
+            <div className="faq_bullet_expand">
+            <p className="answer">{answer}</p>
+            {moreInfoTitle ? <a href={moreInfoLink} className="moreInfo">{moreInfoTitle}</a> : null}
+            </div> : null}
         </div>
     )
 }
