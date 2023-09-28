203c16dea4bb5fbef18e5d4dbdf9ccc643311bc7
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-28 16:17:35 -0400

    Style US UK selector, style more info icon

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,20 +1,41 @@
 import './footer.css'
 import { useState } from 'react'
 import MoreInfo from './MoreInfo'
+import USLogo from '../../images/region/united-states.png'
+import UKLogo from '../../images/region/united-kingdom.png'
+import { AiOutlinePlus, AiOutlineMinus } from 'react-icons/ai'
 
 export default function Footer() {
     const [isExpand, setIsExpand] = useState(false)
+    const [region, setRegion] = useState("US")
 
     function handleClick() {
         setIsExpand(isExpand => !isExpand)
     }
 
+    function handleChange(e) {
+        setRegion(e.target.value)
+    }
+
+    let regionLogo;
+    if (region === "US") {
+        regionLogo = USLogo
+    } else {
+        regionLogo = UKLogo
+    }
+
     return (
         <div className="wrapper__footer">
             <footer className="container__footer">
-                <div className="region_selector">
-                    <div className="selector_dropdown">
-                        Dropdown
+                <div className="wrapper__region_selector">
+                    <div className="region_selector">
+                    <span className="wrapper__region_logo"><img src={regionLogo} className="region_logo"></img></span>
+                    <div>
+                        <select onChange={handleChange} id="region_select">
+                            <option selected value="US">United States</option>
+                            <option value="UK">United Kingdom</option>
+                        </select>
+                    </div>
                     </div>
                     <div className="questions">
                         <p>Questions?</p>
@@ -23,7 +44,7 @@ export default function Footer() {
                 </div>
                 <div className="more_info">
                     <div className="wrapper__btn-round">
-                    <button className="btn-round" onClick={handleClick}>More Info {isExpand ? '-' : '+'}</button>
+                    <button className="btn-round" onClick={handleClick}>More Info {isExpand ? <AiOutlineMinus className="expand_collapse" /> : <AiOutlinePlus className="expand_collapse" />}</button>
                     </div>
                     {isExpand ? 
                     <MoreInfo /> : null}
