ffb9a7e5853c4d064333d33726cb11990b8f4f95
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-28 18:30:19 -0400

    Add brand data

--- client/src/components/home/brands/Brands.js
+++ client/src/components/home/brands/Brands.js
@@ -1,14 +1,25 @@
 import './brands.css'
+import brandsData from './brandsData'
 
 export default function Brands() {
+
+    const allBrands = brandsData.map(item => (
+        <div key={item.brand} className="wrapper__brand">
+            <img src={item.logo} className="brand_logo"></img>
+        </div>
+    ))
     return (
         <div className="wrapper__brands">
             <section className="container__brands">
                 <div className="brands_info">
                     <h2>Let us find your faves from 1000+ brands & styles</h2>
-                    <p>We cary women's sizes XS-3X including Plus, Petite and Maternity; men's sizes XS-3XL including Big & Tall and kids sizes 2T-18.</p>
+                    <p>We carry women's sizes XS-3X including Plus, Petite and Maternity; men's sizes XS-3XL including Big & Tall and kids sizes 2T-18.</p>
                 </div>
-                <div className="brands_list">Swiper</div>
+                <div className="wrapper__brands_list">
+                <div className="container__brands_list">
+                    {allBrands}
+                </div>
+                </div>
             </section>
         </div>
     )
---
+++ client/src/components/home/brands/brandsData.js
@@ -0,0 +1,166 @@
+import adidas from '../../../images/brands/adidas.webp'
+import aldo from '../../../images/brands/aldo.webp'
+import astr from '../../../images/brands/astr.webp'
+import barbour from '../../../images/brands/barbour.webp'
+import beyond_yoga from '../../../images/brands/beyond_yoga.webp'
+import bonobos from '../../../images/brands/bonobos.webp'
+import callaway from '../../../images/brands/callaway.webp'
+import calvin_klein from '../../../images/brands/calvin_klein.webp'
+import club_monaco from '../../../images/brands/club_monaco.webp'
+import columbia from '../../../images/brands/columbia.webp'
+import dolce_vita from '../../../images/brands/dolce_vita.webp'
+import faherty from '../../../images/brands/faherty.webp'
+import farm_rio from '../../../images/brands/farm_rio.webp'
+import frame from '../../../images/brands/frame.webp'
+import free_people from '../../../images/brands/free_people.webp'
+import good_american from '../../../images/brands/good_american.webp'
+import kate_spade from '../../../images/brands/kate_spade.webp'
+import madewell from '../../../images/brands/madewell.webp'
+import marine_layer from '../../../images/brands/marine_layer.webp'
+import mk from '../../../images/brands/mk.webp'
+import mother from '../../../images/brands/mother.webp'
+import new_balance from '../../../images/brands/new_balance.webp'
+import north_face from '../../../images/brands/north_face.webp'
+import paige from '../../../images/brands/paige.webp'
+import rails from '../../../images/brands/rails.webp'
+import sanctuary from '../../../images/brands/sanctuary.webp'
+import sperry from '../../../images/brands/sperry.webp'
+import under_armour from '../../../images/brands/under_armour.webp'
+import vans from '../../../images/brands/vans.webp'
+import vince from '../../../images/brands/vince.webp'
+import vineyard_vines from '../../../images/brands/vineyard_vines.webp'
+import vuori from '../../../images/brands/vuori.webp'
+
+const brandsData = [
+    {
+        brand: "Aldo",
+        logo: aldo
+    },
+    {
+        brand: "Adidas",
+        logo: adidas
+    },
+    {
+        brand: "Astr",
+        logo: astr
+    },
+    {
+        brand: "Barbour",
+        logo: barbour
+    },
+    {
+        brand: "Beyond Yoga",
+        logo: beyond_yoga
+    },
+    {
+        brand: "Bonobos",
+        logo: bonobos
+    },
+    {
+        brand: "Callaway",
+        logo: callaway
+    },
+    {
+        brand: "Calvin Klein",
+        logo: calvin_klein
+    },
+    {
+        brand: "Club Monaco",
+        logo: club_monaco
+    },
+    {
+        brand: "Columbia",
+        logo: columbia
+    },
+    {
+        brand: "Dolce Vita",
+        logo: dolce_vita
+    },
+    {
+        brand: "Faherty",
+        logo: faherty
+    },
+    {
+        brand: "Farm Rio",
+        logo: farm_rio
+    },
+    {
+        brand: "Frame",
+        logo: frame
+    },
+    {
+        brand: "Free People",
+        logo: free_people
+    },
+    {
+        brand: "Good American",
+        logo: good_american
+    },
+    {
+        brand: "Kate Spade",
+        logo: kate_spade
+    },
+    {
+        brand: "Madewell",
+        logo: madewell
+    },
+    {
+        brand: "Marine Layer",
+        logo: marine_layer
+    },
+    {
+        brand: "MK",
+        logo: mk
+    },
+    {
+        brand: "Mother",
+        logo: mother
+    },
+    {
+        brand: "New Balance",
+        logo: new_balance
+    },
+    {
+        brand: "North Face",
+        logo: north_face
+    },
+    {
+        brand: "Paige",
+        logo: paige
+    },
+    {
+        brand: "Rails",
+        logo: rails
+    },
+    {
+        brand: "Sanctuary",
+        logo: sanctuary
+    },
+    {
+        brand: "Sperry",
+        logo: sperry
+    },
+    {
+        brand: "Under Armour",
+        logo: under_armour
+    },
+    {
+        brand: "Vans",
+        logo: vans
+    },
+    {
+        brand: "Vince",
+        logo: vince
+    },
+    {
+        brand: "Vineyard Vines",
+        logo: vineyard_vines
+    },
+    {
+        brand: "Vuori",
+        logo: vuori
+    }
+    
+]
+
+export default brandsData;
