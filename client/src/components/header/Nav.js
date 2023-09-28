3f76da6d9885822185b3fa0b704f03645b6968f5
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-28 09:56:48 -0400

    Add nav menu and SF logo, style icons

--- client/src/components/header/Nav.js
+++ client/src/components/header/Nav.js
@@ -1,9 +1,16 @@
 import './header.css'
+import LOGO from '../../images/logo-round.png'
+import { AiOutlineMenu } from 'react-icons/ai'
 
 export default function Nav() {
     return (
         <div className="container__nav">
-            <span>Nav</span>
+            <div className="wrapper__menu">
+            <AiOutlineMenu className="menu_icon"/>
+            </div>
+            <div className="wrapper__logo">
+            <img src={LOGO} className="logo_circle" alt="Stitch Fix logo"/>
+            </div>
         </div>
     )
 }
