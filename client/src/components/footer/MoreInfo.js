8770cc43f01386fcd37cc27cc057045cbb42f894
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-28 12:23:13 -0400

    Style more info links

--- client/src/components/footer/MoreInfo.js
+++ client/src/components/footer/MoreInfo.js
@@ -1,11 +1,27 @@
 import './moreInfo.css'
+import serviceData from './serviceData'
+import companyData from './companyData'
 
 export default function MoreInfo() {
+
+    const allService = serviceData.map(item => (
+        <a href={item.link} key={item.title}>{item.title}</a>
+    ))
+
+    const allCompany = companyData.map(item => (
+        <a href={item.link} key={item.title}>{item.title}</a>
+    ))
     return (
         <div className="wrapper__more_info">
             <div className="container__service_company_links">
-                <div className="service">The Service</div>
-                <div className="company">The Company</div>
+                <div className="title">
+                <p>The Service</p>
+                <div className="all_links">{allService}</div>
+                </div>
+                <div className="title">
+                <p>The Company</p>
+                <div className="all_links">{allCompany}</div>
+                </div>
             </div>
             <div className="social_links">Social Icons</div>
             <div className="download">Download on App Store</div>
