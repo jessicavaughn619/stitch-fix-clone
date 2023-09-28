595028fda0b183cc627ccdad68094a8cb4a33687
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-28 11:02:51 -0400

    Add footer elements and initial region and more info styling

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,5 +1,24 @@
+import './footer.css'
+
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <div className="wrapper__footer">
+            <footer className="container__footer">
+                <div className="region_selector">
+                    <div className="selector_dropdown">
+                        Dropdown
+                    </div>
+                    <div className="questions">
+                        <p>Questions?</p>
+                        <a href="https://support.stitchfix.com/hc/en-us/sections/360006899113">Visit our FAQ</a>
+                    </div>
+                </div>
+                <div className="more_info">
+                    <button className="btn-round">More Info +</button>
+                </div>
+                <div className="footer_links">
+                Footer Links
+                </div>
+            </footer></div>
     )
 }
