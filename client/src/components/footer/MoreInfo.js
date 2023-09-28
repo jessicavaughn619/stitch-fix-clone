f678fbf0d243e7eb9e1185009755a8dfdd6f1dac
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-28 13:02:54 -0400

    Add social link data and styling

--- client/src/components/footer/MoreInfo.js
+++ client/src/components/footer/MoreInfo.js
@@ -1,6 +1,7 @@
 import './moreInfo.css'
 import serviceData from './serviceData'
 import companyData from './companyData'
+import socialData from './socialData'
 
 export default function MoreInfo() {
 
@@ -11,6 +12,11 @@ export default function MoreInfo() {
     const allCompany = companyData.map(item => (
         <a href={item.link} key={item.title}>{item.title}</a>
     ))
+
+    const allSocial = socialData.map(item => (
+        <a href={item.link} key={item.title}>{item.icon}</a>
+    ))
+
     return (
         <div className="wrapper__more_info">
             <div className="container__service_company_links">
@@ -23,7 +29,11 @@ export default function MoreInfo() {
                 <div className="all_links">{allCompany}</div>
                 </div>
             </div>
-            <div className="social_links">Social Icons</div>
+            <div className="wrapper__social_links">
+                <div className="social_links">
+                    {allSocial}
+                </div>
+            </div>
             <div className="download">Download on App Store</div>
         </div>
     )
---
+++ client/src/components/footer/socialData.js
@@ -0,0 +1,37 @@
+import { BiLogoFacebook, BiLogoInstagram, BiLogoPinterest, BiLogoTiktok, BiLogoYoutube } from 'react-icons/bi'
+import { RiTwitterXLine } from 'react-icons/ri'
+
+const socialData = [
+    {
+        title: "Facebook",
+        icon: <BiLogoFacebook />,
+        link: ""
+    },
+    {
+        title: "Instagram",
+        icon: <BiLogoInstagram />,
+        link: ""
+    },
+    {
+        title: "Pinterest",
+        icon: <BiLogoPinterest />,
+        link: ""
+    },
+    {
+        title: "X",
+        icon: <RiTwitterXLine />,
+        link: ""
+    },
+    {
+        title: "TikTok",
+        icon: <BiLogoTiktok />,
+        link: ""
+    },
+    {
+        title: "YouTube",
+        icon: <BiLogoYoutube />,
+        link: ""
+    }
+]
+
+export default socialData
