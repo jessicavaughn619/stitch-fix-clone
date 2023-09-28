26954d3d4d248d4079401ffbca2fae3b563e363f
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-28 10:17:24 -0400

    Update nav styling, shopping cart icon styling

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -6,6 +6,7 @@ import { BsBag } from 'react-icons/bs'
 export default function Header() {
 
     return (
+        <div className="wrapper__header">
         <header className="container__header">
             <Nav />
             <div className="category-links">
@@ -16,7 +17,10 @@ export default function Header() {
             <NavLink to="/kids" className={({ isActive, isPending }) =>
                     isPending ? "pending" : isActive ? "active" : ""}>Kids</NavLink>
             </div>
-            <BsBag />
+            <div className="wrapper__cart">
+            <BsBag className="cart_icon"/>
+            </div>
         </header>
+        </div>
     )
 }
