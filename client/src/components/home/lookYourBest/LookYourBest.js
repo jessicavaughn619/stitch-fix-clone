bea887a8abd789d2562dd61e4fa9e66f1307305a
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-04 09:59:25 -0600

    Add icons, data, and styling

--- client/src/components/home/lookYourBest/LookYourBest.js
+++ client/src/components/home/lookYourBest/LookYourBest.js
@@ -1,16 +1,29 @@
 import './lookYourBest.css'
+import data from './lookYourBestData'
 
 export default function LookYourBest() {
     return (
         <div className="wrapper__lookYourBest">
-            <section className="container__lookYourBest">
+            <div className="container__lookYourBest">
                 <div className="lookYourBest_info">
                     <h2>We make it easy to look your best, all the time</h2>
                 </div>
-                <div className="lookYourBest_icons">
-                Icons
+                <div className="wrapper__lookYourBest_icons">
+                <section className="container__lookYourBest_icons">
+                <div className="lookYourBest_grid_container">
+                {data.map(data => 
+                    <div className="lookYourBest_grid_item" id={data.id}>
+                        <div className="icon">
+                            <img src={data.icon} alt={data.heading}></img>
+                        </div>
+                        <p className="icon_heading">{data.heading}</p>
+                        <p className="icon_content">{data.content}</p>
+                    </div>
+                )}
                 </div>
-            </section>
+                </section>
+                </div>
+            </div>
         </div>
     )
 }
---
+++ client/src/components/home/lookYourBest/lookYourBestData.js
@@ -0,0 +1,26 @@
+import IMG1 from './images/expert-picks.png'
+import IMG2 from './images/free-shipping.png'
+import IMG3 from './images/no-commitment.png'
+
+const data = [
+    {
+        id: 1,
+        icon: IMG1,
+        heading: "Expert picks",
+        content: "Get outfit and styling suggestions that meet your price preferences."
+    },
+    {
+        id: 2,
+        icon: IMG2,
+        heading: "Free shipping",
+        content: "Easy returns and exchanges on all orders, pre-paid labels included."
+    },
+    {
+        id: 3,
+        icon: IMG3,
+        heading: "No commitment",
+        content: "Shop when you want, how you want. No subscription required."
+    }
+]
+
+export default data;
