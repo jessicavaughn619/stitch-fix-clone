9006095bcdf71a453be148ddef2328b6f0e4e41c
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-04 11:59:29 -0600

    Add header content and render conditionally based on route

--- client/src/components/home/lookYourBest/LookYourBest.js
+++ client/src/components/home/lookYourBest/LookYourBest.js
@@ -1,18 +1,36 @@
 import './lookYourBest.css'
 import data from './lookYourBestData'
+import headerData from './lookYourBestHeaderData'
+import { useLocation } from 'react-router-dom'
 
 export default function LookYourBest() {
+    const location = useLocation()
+
+    let headerText;
+    let headerObject;
+    if (location.pathname === "/women") {
+        headerObject = headerData.find(item => item.style === "women")
+        headerText = headerObject.content
+    } else if (location.pathname === "/men") {
+        headerObject = headerData.find(item => item.style === "men")
+        headerText = headerObject.content
+    }
+    else {
+        headerObject = headerData.find(item => item.style === "home")
+        headerText = headerObject.content
+    }
+
     return (
         <div className="wrapper__lookYourBest">
             <div className="container__lookYourBest">
                 <div className="lookYourBest_info">
-                    <h2>We make it easy to look your best, all the time</h2>
+                    <h2>{headerText}</h2>
                 </div>
                 <div className="wrapper__lookYourBest_icons">
                 <section className="container__lookYourBest_icons">
                 <div className="lookYourBest_grid_container">
                 {data.map(data => 
-                    <div className="lookYourBest_grid_item" id={data.id}>
+                    <div className="lookYourBest_grid_item" key={data.id}>
                         <div className="icon">
                             <img src={data.icon} alt={data.heading}></img>
                         </div>
---
+++ client/src/components/home/lookYourBest/lookYourBestHeaderData.js
@@ -0,0 +1,19 @@
+const headerData = [
+    {
+        id: 1,
+        style: "home",
+        content: "We make it easy to look your best, all the time"
+    },
+    {
+        id: 2,
+        style: "women",
+        content: "We make it easy to build your dream closet"
+    },
+    {
+        id: 3,
+        style: "men",
+        content: "We make it easy to keep your style game looking strong"
+    }
+]
+
+export default headerData;
