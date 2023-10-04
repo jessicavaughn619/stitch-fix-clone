f9d6c2262757765ad49e5449bb755a8705f8c124
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-04 17:32:29 -0600

    Style container and fonts

--- client/src/components/home/styles/Styles.js
+++ client/src/components/home/styles/Styles.js
@@ -8,7 +8,7 @@ export default function Styles() {
                 <h2>Tell us what you like, we'll find what you love</h2>
                 <p>We get to know your style, fit and price range based on your style quiz, requests and feedback.</p>
             </div>
-            <div className="styles_swiper">
+            <div className="container__styles_swiper">
                 Swiper
             </div>
         </section>
