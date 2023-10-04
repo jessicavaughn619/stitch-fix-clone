05710f4f90108f5183a2446ffaaa475c5cfe7c46
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-04 10:51:22 -0600

    Revert render-build, add ../api

--- api/bin/render-build.sh
+++ api/bin/render-build.sh
@@ -2,12 +2,10 @@
 # exit on error
 set -o errexit
 
-cd ./client
-
 # builds the front end code
-rm -rf ../public
+rm -rf public
 npm install --prefix client && npm run build --prefix client
-cp -a client/build/. ../public/
+cp -a client/build/. public/
 
 cd ../api
 
