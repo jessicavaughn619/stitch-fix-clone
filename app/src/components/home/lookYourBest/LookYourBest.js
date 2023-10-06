0f7564d49d4ef8fc7120553f9eafb29facfd8d7b
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-06 10:26:55 -0600

    Refactor data into one file

--- app/src/components/home/lookYourBest/LookYourBest.js
+++ app/src/components/home/lookYourBest/LookYourBest.js
@@ -1,44 +1,42 @@
 import './lookYourBest.css'
 import data from './lookYourBestData'
-import headerData from './lookYourBestHeaderData'
 import { useLocation } from 'react-router-dom'
 
 export default function LookYourBest() {
     const location = useLocation()
 
-    let headerText;
     let headerObject;
     
     switch (location.pathname) {
       case "/women":
-        headerObject = headerData.find(item => item.style === "women");
+        headerObject = data.find(item => item.style === "women");
         break;
       case "/men":
-        headerObject = headerData.find(item => item.style === "men");
+        headerObject = data.find(item => item.style === "men");
         break;
       default:
-        headerObject = headerData.find(item => item.style === "home");
+        headerObject = data.find(item => item.style === "home");
         break;
     }
     
-    headerText = headerObject.content;
+    const { title, steps } = headerObject;
 
     return (
         <div className="wrapper__lookYourBest">
             <div className="container__lookYourBest">
                 <div className="lookYourBest_info">
-                    <h2>{headerText}</h2>
+                    <h2>{title}</h2>
                 </div>
                 <div className="wrapper__lookYourBest_icons">
                 <section className="container__lookYourBest_icons">
                 <div className="lookYourBest_grid_container">
-                {data.map(data => 
-                    <div className="lookYourBest_grid_item" key={data.id}>
+                {steps.map(step => 
+                    <div className="lookYourBest_grid_item" key={step.id}>
                         <div className="icon">
-                            <img src={data.icon} alt={data.heading}></img>
+                            <img src={step.icon} alt={step.heading}></img>
                         </div>
-                        <p className="icon_heading">{data.heading}</p>
-                        <p className="icon_content">{data.content}</p>
+                        <p className="icon_heading">{step.heading}</p>
+                        <p className="icon_content">{step.content}</p>
                     </div>
                 )}
                 </div>
--- app/src/components/home/lookYourBest/lookYourBestData.js
+++ app/src/components/home/lookYourBest/lookYourBestData.js
@@ -5,21 +5,72 @@ import IMG3 from './images/no-commitment.png'
 const data = [
     {
         id: 1,
-        icon: IMG1,
-        heading: "Expert picks",
-        content: "Get outfit and styling suggestions that meet your price preferences."
+        style: "home",
+        title: "We make it easy to look your best, all the time",
+        steps: [{
+            id: 1,
+            icon: IMG1,
+            heading: "Expert picks",
+            content: "Get outfit and styling suggestions that meet your price preferences."
+        },
+        {
+            id: 2,
+            icon: IMG2,
+            heading: "Free shipping",
+            content: "Easy returns and exchanges on all orders, pre-paid labels included."
+        },
+        {
+            id: 3,
+            icon: IMG3,
+            heading: "No commitment",
+            content: "Shop when you want, how you want. No subscription required."
+        }]
     },
     {
         id: 2,
-        icon: IMG2,
-        heading: "Free shipping",
-        content: "Easy returns and exchanges on all orders, pre-paid labels included."
+        style: "women",
+        title: "We make it easy to build your dream closet",
+        steps: [{
+            id: 1,
+            icon: IMG1,
+            heading: "Expert picks",
+            content: "Get outfit and styling suggestions that meet your price preferences."
+        },
+        {
+            id: 2,
+            icon: IMG2,
+            heading: "Free shipping",
+            content: "Easy returns and exchanges on all orders, pre-paid labels included."
+        },
+        {
+            id: 3,
+            icon: IMG3,
+            heading: "No commitment",
+            content: "Shop when you want, how you want. No subscription required."
+        }]
     },
     {
         id: 3,
-        icon: IMG3,
-        heading: "No commitment",
-        content: "Shop when you want, how you want. No subscription required."
+        style: "men",
+        title: "We make it easy to keep your style game looking strong",
+        steps: [{
+            id: 1,
+            icon: IMG1,
+            heading: "Expert picks",
+            content: "Get outfit and styling suggestions that meet your price preferences."
+        },
+        {
+            id: 2,
+            icon: IMG2,
+            heading: "Free shipping",
+            content: "Easy returns and exchanges on all orders, pre-paid labels included."
+        },
+        {
+            id: 3,
+            icon: IMG3,
+            heading: "No commitment",
+            content: "Shop when you want, how you want. No subscription required."
+        }]
     }
 ]
 
