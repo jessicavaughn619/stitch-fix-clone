2327564f9d216278153564e6890df8ca20817977
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-06 14:03:20 -0600

    Add spacing between footer links

--- app/src/components/footer/Footer.js
+++ app/src/components/footer/Footer.js
@@ -55,11 +55,11 @@ export default function Footer() {
                 </div>
                 <div className="wrapper__footer_links">
                     <div className="container__footer_links">
-                       <a href="https://www.stitchfix.com/terms" className="footer_link">Terms of Use</a>-
-                        <a href="https://www.stitchfix.com/privacy" className="footer_link">Privacy Policy</a>-
-                        <a href="https://www.stitchfix.com/supply-chain-information" className="footer_link">Supply Chain Information</a>-
-                        <a href="https://www.stitchfix.com/privacy#marketing-companies" className="footer_link">Ad Choices</a>-
-                        <a href="https://www.stitchfix.com/sitemap" className="footer_link">Sitemap</a>
+                       <a href="https://www.stitchfix.com/terms" className="footer_link">Terms of Use</a> - 
+                        <a href="https://www.stitchfix.com/privacy" className="footer_link"> Privacy Policy</a> - 
+                        <a href="https://www.stitchfix.com/supply-chain-information" className="footer_link"> Supply Chain Information</a> -
+                        <a href="https://www.stitchfix.com/privacy#marketing-companies" className="footer_link"> Ad Choices</a> -
+                        <a href="https://www.stitchfix.com/sitemap" className="footer_link"> Sitemap</a>
                     </div>
                     <p className="trademark">Stitch Fix and Fix are trademarks of Stitch Fix, Inc.</p>
                 </div>
