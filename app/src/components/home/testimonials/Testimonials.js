f9810fab1b7e8a0e498ef2280c5d01f22fc107ed
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-06 10:17:00 -0600

    Refactor image and imageAlt into data file

--- app/src/components/home/testimonials/Testimonials.js
+++ app/src/components/home/testimonials/Testimonials.js
@@ -1,5 +1,4 @@
 import './testimonials.css'
-import IMG1 from './images/testimonials.webp'
 import data from './testimonialData'
 import { useLocation } from 'react-router-dom'
 
@@ -17,14 +16,14 @@ export default function Testimonials() {
         break;
     }
 
-    const { title, content } = headerObject;
+    const { title, content, image, imageAlt } = headerObject;
 
     return (
         <div className="wrapper__testimonials">
         <section className="container__testimonials">
             <div className="container__image_info">
             <div className="wrapper__image">
-                <img src={IMG1} alt="A man and women Stitch Fix stylist picking clothing from a rack."/>
+                <img src={image} alt={imageAlt}/>
             </div>
             <div className="testimonials_info">
                 <h2>{title}</h2>
--- app/src/components/home/testimonials/testimonialData.js
+++ app/src/components/home/testimonials/testimonialData.js
@@ -1,13 +1,19 @@
+import IMG1 from './images/testimonials.webp'
+
 const data = [
     {
         id: 1,
         style: "home",
+        image: IMG1,
+        imageAlt: "A man and women Stitch Fix stylist picking clothing from a rack.",
         title: "I'd like to thank my Stylist...",
         content: "...for really getting my style. Your Stylist helps you dress for the changing seasons, recommends pieces that fit your unique body and incorporates your input into each piece they send."
     },
     {
         id: 2,
         style: "women",
+        image: IMG1,
+        imageAlt: "A man and women Stitch Fix stylist picking clothing from a rack.",
         title: "I'd like to thank my Stylist...",
         content: "...for really getting my style. Your Stylist helps you dress for the changing seasons, recommends pieces that fit your unique body and incorporates your input into each piece they send."
     }
