a55cff35b1254371121a5389ff903f9d123d0f04
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-06 10:14:22 -0600

    Add testimonialData file with title and content

--- app/src/components/home/testimonials/Testimonials.js
+++ app/src/components/home/testimonials/Testimonials.js
@@ -1,7 +1,24 @@
 import './testimonials.css'
 import IMG1 from './images/testimonials.webp'
+import data from './testimonialData'
+import { useLocation } from 'react-router-dom'
 
 export default function Testimonials() {
+    const location = useLocation();
+
+    let headerObject;
+    
+    switch (location.pathname) {
+      case "/women":
+        headerObject = data.find(item => item.style === "women");
+        break;
+      default:
+        headerObject = data.find(item => item.style === "home");
+        break;
+    }
+
+    const { title, content } = headerObject;
+
     return (
         <div className="wrapper__testimonials">
         <section className="container__testimonials">
@@ -10,9 +27,8 @@ export default function Testimonials() {
                 <img src={IMG1} alt="A man and women Stitch Fix stylist picking clothing from a rack."/>
             </div>
             <div className="testimonials_info">
-                <h2>I'd like to thank my Stylist...</h2>
-                <p>...for really getting my style. Your Stylist helps you dress for the changing seasons,
-                recommends pieces that fit your unique body and incorporates your input into each piece they send.</p>
+                <h2>{title}</h2>
+                <p>{content}</p>
             </div>
             </div>
             <div className="testimonials_swiper">
---
+++ app/src/components/home/testimonials/testimonialData.js
@@ -0,0 +1,16 @@
+const data = [
+    {
+        id: 1,
+        style: "home",
+        title: "I'd like to thank my Stylist...",
+        content: "...for really getting my style. Your Stylist helps you dress for the changing seasons, recommends pieces that fit your unique body and incorporates your input into each piece they send."
+    },
+    {
+        id: 2,
+        style: "women",
+        title: "I'd like to thank my Stylist...",
+        content: "...for really getting my style. Your Stylist helps you dress for the changing seasons, recommends pieces that fit your unique body and incorporates your input into each piece they send."
+    }
+]
+
+export default data;
