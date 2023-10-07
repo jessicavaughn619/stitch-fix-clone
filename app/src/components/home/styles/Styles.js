70ae60295c0bdd2f1b6a3a26a36584b2dadce08d
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-06 21:02:17 -0600

    Add space between slides, render different slides per view conditionally based on route

--- app/src/components/home/styles/Styles.js
+++ app/src/components/home/styles/Styles.js
@@ -32,7 +32,7 @@ export default function Styles() {
             </div>
             <div className="wrapper__styles_swiper">
                 <div className="container__styles_swiper">
-                    <swiper-container slides-per-view="1.5" id="swiper-container" navigation="true">
+                    <swiper-container slides-per-view={location.pathname === "/" ? "1.5" : "1"} space-between="8" navigation="true" >
                         {stylesData.slides.map(slide => (
                             <swiper-slide 
                             key={slide.id}>
@@ -42,12 +42,12 @@ export default function Styles() {
                             <img src={slide.image} alt={slide.imageAlt}></img>
                             </div>
                             </a>
-                            <div className="styles_label">
+                            {(location.pathname === "/") ? <div className="styles_label">
                                 <a className="styles_link" href={slide.link}>
                                     <span className="styles_link_content">{slide.style}</span>
                                     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" color="currentColor"><path fillRule="evenodd" clipRule="evenodd" d="M10.7699 8.69226L6.90066 12.5011L7.88278 13.4988L12.9699 8.49111L13.4767 7.99225L12.9699 7.49339L7.89818 2.5011L6.91608 3.49883L10.7699 7.29226H2.52036V8.69226H10.7699Z" fill="#00625d"></path></svg>
                                 </a>
-                            </div>
+                            </div> : null}
                             </div>
                             </swiper-slide>
                         ))}
