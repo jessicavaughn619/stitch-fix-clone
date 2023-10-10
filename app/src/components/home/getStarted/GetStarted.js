20ca6b8c6e691d402b6e28abcdb6d27f30f2dae4
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-10 10:15:33 -0600

    Added getStartedData file to conditionally render data based on route

--- app/src/components/home/getStarted/GetStarted.js
+++ app/src/components/home/getStarted/GetStarted.js
@@ -1,13 +1,36 @@
 import './getStarted.css'
+import data from './getStartedData'
+import {useLocation} from 'react-router-dom'
 
 export default function GetStarted() {
+    const location = useLocation();
+
+    let dataObject;
+    
+    switch (location.pathname) {
+      case "/women":
+        dataObject = data.find(item => item.style === "women");
+        break;
+      case "/men":
+        dataObject = data.find(item => item.style === "men");
+        break;
+        case "/kids":
+            dataObject = data.find(item => item.style === "kids");
+            break;
+      default:
+        dataObject = data.find(item => item.style === "home");
+        break;
+    }
+
+    const { heading, buttonText } = dataObject;
+
     return (
         <div className="wrapper__getStarted">
             <section className="container__getStarted">
                 <div className="getStarted_info">
-                    <h2>Ready to get started with Stitch Fix?</h2>
+                    <h2>{heading}</h2>
                 </div>
-                <button className="btn">Take your style quiz</button>
+                <button className="btn">{buttonText}</button>
             </section>
         </div>
     )
---
+++ app/src/components/home/getStarted/getStartedData.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id: 1,
+        style: "home",
+        heading: "Ready to get started with Stitch Fix?",
+        buttonText: "Take your style quiz"
+    },
+    {
+        id: 2,
+        style: "women",
+        heading: "Ready to get started with Stitch Fix?",
+        buttonText: "Take your style quiz"
+    },
+    {
+        id: 3,
+        style: "men",
+        heading: "Ready to get started with Stitch Fix?",
+        buttonText: "Take your style quiz"
+    },
+    {
+        id: 4,
+        style: "kids",
+        heading: "Ready to get started with Stitch Fix?",
+        buttonText: "Take their style quiz"
+    }
+]
+
+export default data;
