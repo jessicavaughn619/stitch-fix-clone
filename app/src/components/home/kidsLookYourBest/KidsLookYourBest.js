31da9adc4d00ef44cc081358ee36a95c8eeabf1b
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-10 10:01:15 -0600

    Add data, style component

--- app/src/components/home/kidsLookYourBest/KidsLookYourBest.js
+++ app/src/components/home/kidsLookYourBest/KidsLookYourBest.js
@@ -1,8 +1,40 @@
+import './kidsLookYourBest.css'
+import data from './kidsLookYourBestData'
+import IMG1 from './images/kids_style.webp'
+
 export default function KidsLookYourBest() {
+
+    let kidsContent;
+
+    kidsContent = data.find(item => item.style === "kids")
+
+    const { title, steps } = kidsContent;
+
     return (
         <div className="wrapper__kids_look_your_best">
-            <section>
-                Kids
+            <section className="container__kids_look_your_best">
+                <div className="wrapper__kids_image">
+                    <div className="container__kids_image">
+                        <img src={IMG1} alt="A lay down of kid's clothing and shoes from Stitch Fix."></img>
+                    </div>
+                </div>
+                <div className="wrapper__kids_content">
+                    <div className="container__kids_content">
+                        <div className="kids_content_title">
+                            <h2>{title}</h2>
+                        </div>
+                        <div className="wrapper__kids_content_steps">
+                            <div className="kids_content_steps">
+                            {steps.map(step => (
+                                <div className="content_step" key={step.id}>
+                                    <h3>{step.heading}</h3>
+                                    <p>{step.content}</p>
+                                </div>
+                            ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </section>
         </div>
     )
---
+++ app/src/components/home/kidsLookYourBest/kidsLookYourBestData.js
@@ -0,0 +1,26 @@
+const data = [
+    {
+        id: 1,
+        style: "kids",
+        title: "Easy styles for the kiddos, starting at $10",
+        steps: [
+            {
+                id: 1,
+                heading: "The right fit",
+                content: "We stock lots of back-to-school styles in sizes 2T-18."
+            },
+            {
+                id: 2,
+                heading: "Fun finds",
+                content: "What's better than a box of clothes picked just for them? When you keep at least one item in your Fix, the $20 styling fee is credited toward your total. Love them all? Get 25% off."
+            },
+            {
+                id: 3,
+                heading: "Time saver",
+                content: "Back-to-school shopping, all in one place, delivered right to your door. Their good-to-go outfits will make morning way easier, too."
+            }
+        ]
+    }
+]
+
+export default data;
