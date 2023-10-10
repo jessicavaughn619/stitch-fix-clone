1cbf681c5d72121d065b70200fdacbbc71e66932
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-10 10:09:20 -0600

    Update kids how it works styling

--- app/src/components/home/howItWorks/Step.js
+++ app/src/components/home/howItWorks/Step.js
@@ -1,15 +1,17 @@
 import './howItWorks.css'
+import { useLocation } from 'react-router-dom';
 
 export default function Step({step}) {
     const { image, imgAlt, heading, detail } = step;
+    const location = useLocation();
 
     return (
         <div className="container_image_content">
         {image ? <div className="image-wrapper">
             <img src={image} alt={imgAlt}></img>
         </div> : null}
-        <p className="heading">{heading}</p>
-        <p className="detail">{detail}</p>
+        <p className={location.pathname === "/kids" ? "kids_heading" : "heading"}>{heading}</p>
+        <p className={location.pathname === "/kids" ? "kids_detail" : "detail"}>{detail}</p>
     </div>
     )
 }
