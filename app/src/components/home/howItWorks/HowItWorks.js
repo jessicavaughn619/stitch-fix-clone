03e54857469c78c86c4eb6b4a2e612bb2c395830
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-10 09:15:59 -0600

    Add kids wrapper

--- app/src/components/home/howItWorks/HowItWorks.js
+++ app/src/components/home/howItWorks/HowItWorks.js
@@ -12,7 +12,7 @@ const allSteps = data.map(step => (
         />
     ))
     return (
-        <div className="wrapper__how_it_works">
+        <div className={location.pathname === "/kids" ? "wrapper__how_it_works_kids" : "wrapper__how_it_works"}>
         <section className={location.pathname === "/kids" ? "container__how_it_works_kids" : "container__how_it_works"}>
             <h2>{location.pathname === "/kids" ? "How it works" : "How Stitch Fix works"}</h2>
             <div className="container_steps">
