229d3e284fdcd786b076aafec850658cccb83cfe
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-11 12:08:38 -0400

    Style footer links, remove CA and Privacy links

--- app/src/components/footer/FooterLinks.js
+++ app/src/components/footer/FooterLinks.js
@@ -7,8 +7,6 @@ export default function FooterLinks() {
             <a href="https://www.stitchfix.com/privacy" className="footer_link"> Privacy Policy</a> - 
             <a href="https://www.stitchfix.com/supply-chain-information" className="footer_link"> Supply Chain Information</a> -
             <a href="https://www.stitchfix.com/privacy#marketing-companies" className="footer_link"> Ad Choices</a> -
-            <a href="https://www.stitchfix.com/privacy#ca-notice" className="footer_link"> CA Notice at Collection</a> -
-            <a href="" className="footer_link"> Your Privacy Choices</a> -
             <a href="https://www.stitchfix.com/sitemap" className="footer_link"> Sitemap</a>
         </div>
         <p className="trademark">Stitch Fix and Fix are trademarks of Stitch Fix, Inc.</p>
