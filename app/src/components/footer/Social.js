9351b1b79f64d4663073a668bf3f306d014cb08b
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-10-12 12:05:35 -0400

    Add hover state to all social buttons

--- app/src/components/footer/Social.js
+++ app/src/components/footer/Social.js
@@ -10,7 +10,7 @@ export default function Social({ item, onSetIsSocialExpand, activeSocial }) {
     return (
         <li className={`social_link_li ${(activeSocial.id === id) ? "active" : "inactive"}`}>
         {handles ? <button className="social_link_button" onClick={handleClick}>{icon}</button> : 
-        <a className="social_link_link" href={link}>{icon}</a>}
+        <a className="social_link_link social_link_button" href={link}>{icon}</a>}
     </li>
     )
 }
